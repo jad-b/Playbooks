345a31b974abe15a7a99c45c20244da70e7f5a83
jad-b <j.american.db@gmail.com>
2015-05-11 10:02:54 -0400

    Add python-dev and fix run.sh source in bootstrap.sh
    * Python-dev is needed for compiling Ansible's PyYAML dependency correctly

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,10 +1,10 @@
 #!/bin/sh -e
 
 # Install pip
-sudo apt-get install python-pip
+sudo apt-get install python-pip python-dev
 
 # Install ansible
 pip install --user -U ansible
 
 # Run the ansible playbooks
-./run.sh
+. run.sh
