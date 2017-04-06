3d5616fe707271bc5839efc8a0e6a59c2b014ad0
jad-b <j.american.db@gmail.com>
2017-04-06 17:35:50 -0400

    Install FF dev edition; upgrade all apt packages

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,10 +1,14 @@
-#!/bin/sh -e
+#!/bin/sh -eux
 
 # Install pip
-sudo apt-get install python-pip python-dev libssl-dev
+sudo apt-get install \
+	aptitude \
+	libssl-dev \
+	python-dev \
+	python-pip
 
 # Install ansible
 pip install --user -U ansible
 
 # Run the ansible playbooks
-./run.sh
+./run.sh "$@"
